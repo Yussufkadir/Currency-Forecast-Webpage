88af0ac1bc0eb6fbb4ae8046a9962f2d1b6ef39f
Yussufkadir <ysyurme1@stu.vistula.edu.pl>
2025-10-13 11:52:28 +0200

    fix: minor fix in backend application.

--- backend/backend.py
+++ backend/backend.py
@@ -53,7 +53,7 @@ def load_models():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
